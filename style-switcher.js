2dbe0face11139a7ba3dcc512d836d94d6ea714b
ANKIT <bansalankit8447@gmail.com>
2024-04-12 00:26:34 +0530

    changes done

--- style-switcher.js
+++ style-switcher.js
@@ -14,7 +14,7 @@ const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color) {
     alternateStyles.forEach((style) => {
         if(color == style.getAttribute("title")) {
-            style.removeAttribute("disabled");
+            style.removeAttribute("disabled"); 
         }
         else{
             style.setAttribute("disabled","true");
